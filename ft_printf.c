a15127050e50d654c8ffe83c3abbe8875b4af608
Sergio Nunez <snunez@c1r7s1.42madrid.com>
2021-03-23 17:00:59 +0100

    V2

--- ft_printf.c
+++ ft_printf.c
@@ -6,39 +6,43 @@
 /*   By: snunez <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/19 11:12:10 by snunez            #+#    #+#             */
-/*   Updated: 2021/03/19 13:31:36 by snunez           ###   ########.fr       */
+/*   Updated: 2021/03/23 17:00:17 by snunez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Libft/libft.h"
 #include "ft_printf.h"
 
-void	ft_printdata(char *data, t_args arg)
+void	ft_tratar_flag(const char *format, int pos, char *flags, t_flags args)
 {
-	int	i;
-	while (*data + i)
+	char	*aux;
+
+	aux = (char *)format;
+	while (*(aux + pos) && ft_isalpha(*aux + i) == 0)
 	{
-		if ()
+
+		pos++;
 	}
 }
 
 void	analysis(const char *format, t_args args, int pos)
 {
-	char *flags = "-0.*";
+	char *flags = "-0.*+";
 	char *datos = "cspdiuxX";
+
 	while (*format + pos)
 	{
 		if (ft_strchr(flags, *(format + pos)))
-			ft_tratar_flag();
+			ft_tratar_flag(format, pos, args);
 		else if (ft_strchr(datos, *(format + pos)))
 		{
-			ft_imprimir datos();
+			ft_putchar_fd(*(format + pos), 1);
 			break;
 		}
 		else if (*(format + pos) == '%')
 			ft_putchar_fd('%', 1);
 		else
-			escapatrajo();
+			error();
 		pos++;
 	}
 }
@@ -57,4 +61,5 @@ int	ft_printf(const char *format, ...)
 		}
 		i++;
 	}
+	return ();
 }
